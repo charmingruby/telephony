a18073e71061b14d79e1b7597c5aba5530dc75db
charmingruby <mrrubycanal3105@gmail.com>
2024-06-27 09:52:19 -0300

    feat: create guild table

---
+++ db/migrations/000002_create_guilds_table.up.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS guilds
+(
+    id bigserial PRIMARY KEY NOT NULL,
+    name varchar NOT NULL,
+    description varchar NOT NULL,
+    tags varchar[] NOT NULL,
+    channels_quantity integer NOT NULL,
+    profile_id integer REFERENCES users_profile (id),
+    created_at timestamp DEFAULT now() NOT NULL,
+    updated_at timestamp DEFAULT now() NOT NULL,
+    deleted_at timestamp
+);
