ea07bbdccc88a6c44018dc8fa72ce844e766317a
charmingruby <mrrubycanal3105@gmail.com>
2024-06-29 09:21:59 -0300

    feat: channel entity

---
+++ internal/domain/guild/entity/channel.go
@@ -0,0 +1,36 @@
+package entity
+
+import (
+	"time"
+
+	"github.com/charmingruby/telephony/internal/core"
+	"github.com/charmingruby/telephony/internal/validation"
+)
+
+func NewChannel(name string, profileID int) (*Channel, error) {
+	channel := Channel{
+		ID:               core.NewDefaultDomainID(),
+		Name:             name,
+		MessagesQuantity: 0,
+		OwnerID:          profileID,
+		CreatedAt:        time.Now(),
+		UpdatedAt:        time.Now(),
+		DeletedAt:        nil,
+	}
+
+	if err := validation.ValidateStruct(channel); err != nil {
+		return nil, err
+	}
+
+	return &channel, nil
+}
+
+type Channel struct {
+	ID               int        `json:"id" validate:"required" db:"id"`
+	Name             string     `json:"name" validate:"required,min=1,max=36" db:"name"`
+	MessagesQuantity int        `json:"messages_quantity" db:"messages_quantity"`
+	OwnerID          int        `json:"owner_id" validate:"required" db:"owner_id"`
+	CreatedAt        time.Time  `json:"created_at" validate:"required" db:"created_at"`
+	UpdatedAt        time.Time  `json:"updated_at" validate:"required" db:"updated_at"`
+	DeletedAt        *time.Time `json:"deleted_at" db:"deleted_at"`
+}
---
+++ internal/domain/guild/entity/channel_test.go
@@ -0,0 +1,33 @@
+package entity
+
+import (
+	"testing"
+
+	"github.com/charmingruby/telephony/internal/core"
+	"github.com/charmingruby/telephony/internal/validation"
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_NewChannel(t *testing.T) {
+	name := "dummy name"
+	profileID := 2
+
+	t.Run("it should be able to create a channel", func(t *testing.T) {
+		c, err := NewChannel(name, profileID)
+
+		assert.NoError(t, err)
+		assert.Equal(t, core.NewDefaultDomainID(), c.ID)
+		assert.Equal(t, name, c.Name)
+		assert.Equal(t, 0, c.MessagesQuantity)
+		assert.Equal(t, profileID, c.OwnerID)
+		assert.Nil(t, c.DeletedAt)
+	})
+
+	t.Run("it should receive an error when try to create a channel with invalid params", func(t *testing.T) {
+		c, err := NewChannel("", profileID)
+
+		assert.Nil(t, c)
+		assert.Error(t, err)
+		assert.Equal(t, validation.NewValidationErr(validation.ErrRequired("name")).Error(), err.Error())
+	})
+}
